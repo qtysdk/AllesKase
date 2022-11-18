3dc5f45c4e4d6fd040cf27e671e7eb4ee6f24f60
Ching Yi, Chan <qrtt1@infuseai.io>
2022-11-19 00:10:43 +0800

    Implement JoinGame api and use-case
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- src/main/kotlin/gaas/common/Exceptions.kt
+++ src/main/kotlin/gaas/common/Exceptions.kt
@@ -3,6 +3,7 @@ package gaas.common
 
 class IllegalGameStateException(message: String) : RuntimeException(message)
 
+// For StartGameUseCase
 val CannotStartGameWithTooFewPlayersException = IllegalGameStateException("TOO_FEW_PLAYER")
 val CannotStartGameByNonHostPlayerException = IllegalGameStateException("ONLY_HOST_PLAY_CAN_DO_THIS")
 val GameDoesNotExistException = IllegalGameStateException("NO_SUCH_GAME_ID")
@@ -10,6 +11,14 @@ val GameHasStartedException = IllegalGameStateException("GAME_HAS_ALREADY_STARTE
 val GameHasFinishedException = IllegalGameStateException("GAME_HAS_ALREADY_FINISHED")
 
 val PlayerHasBeenDead = IllegalGameStateException("PLAYER_HAS_BEEN_DEAD")
+
+// For JoinGameUseCase
+val GameRoomHasBeenFull = IllegalGameStateException("GAME_ROOM_HAS_BEEN_FULL")
+val GameRoomHasStarted = IllegalGameStateException("GAME_ROOM_HAS_STARED")
+val PlayerHasBeenInTheGame = IllegalGameStateException("PLAYER_HAS_BEEN_IN_THE_GAME")
+
+
+// For PlayerActionUseCase
 
 class IllegalPlayerActionException(message: String) : RuntimeException(message)
 
--- src/main/kotlin/gaas/usecases/JoinGameUseCase.kt
+++ src/main/kotlin/gaas/usecases/JoinGameUseCase.kt
@@ -1,5 +1,6 @@
 package gaas.usecases
 
+import gaas.common.PlayerHasBeenInTheGame
 import gaas.domain.Game
 import gaas.repository.Database
 
@@ -11,9 +12,15 @@ interface JoinGameUseCase {
 class JoinGameUseCaseImpl(private val database: Database) : JoinGameUseCase {
     override fun join(gameId: String, playerId: String): Boolean {
         val game: Game = database.findGameById(gameId)!!
-
-        // TODO check player exists or do exception handleing
-        game.join(database.findPlayerById(playerId)!!)
+        try {
+            game.join(database.findPlayerById(playerId)!!)
+        } catch (e: Exception) {
+            // duplicated join, just return false enough
+            if (e == PlayerHasBeenInTheGame) {
+                return false
+            }
+            throw e
+        }
         return true
     }
 
